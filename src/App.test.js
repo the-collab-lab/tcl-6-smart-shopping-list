3c1ee6c1ce735d98d76c460dce658993e270c2a8
Scott Bergler <commandinghands@gmail.com>
2020-04-08 09:55:02 -0700

    Import FirestoreProvider.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FirestoreProvider } from 'react-firestore';
+import { fb } from './lib/firebase';
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <FirestoreProvider firebase={fb}>
+      <App />
+    </FirestoreProvider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
