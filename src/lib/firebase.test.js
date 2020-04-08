fcbf3e3b81d360faea97d9dcf135ec65c8b55e91
Scott Bergler <commandinghands@gmail.com>
2020-04-08 10:47:04 -0700

    Render AddTestItem component.

--- src/lib/firebase.test.js
+++ src/lib/firebase.test.js
@@ -1,5 +1,69 @@
 import React from 'react';
+import { db } from './firebase';
 import { FirestoreCollection } from 'react-firestore';
+
+class AddTestItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', category: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({ name: event.target.value });
+  }
+
+  handleNameChange(event) {
+    console.log(event.target.value);
+    this.setState({ name: event.target.value });
+  }
+
+  handleCategoryChange(event) {
+    console.log(event.target.value);
+    this.setState({ category: event.target.value });
+  }
+
+  handleSubmit(event) {
+    db.collection('testitems')
+      .add(this.state)
+      .then(function(docRef) {
+        console.log('Document written with ID: ', docRef.id);
+      })
+      .catch(function(error) {
+        console.error('Error adding document: ', error);
+      });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
+        </label>
+        <label>
+          Category:
+          <input
+            type="text"
+            value={this.state.category}
+            onChange={this.handleCategoryChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 function TestList() {
   return (
@@ -24,7 +88,7 @@ function TestList() {
   );
 }
 
-export default TestList;
+export { TestList, AddTestItem };
 
 // describe('firebase reads and writes', () => {
 //   it('writes to the database', () => {
@@ -33,15 +97,15 @@ export default TestList;
 //       name: 'limes',
 //       category: 'citrus',
 //     };
-//     return db
-//       .collection(collectionName)
-//       .add(groceryItem)
-//       .then(function(docRef) {
-//         console.log('Document written with ID: ', docRef.id);
-//       })
-//       .catch(function(error) {
-//         console.error('Error adding document: ', error);
-//       });
+// return db
+//   .collection(collectionName)
+//   .add(groceryItem)
+//   .then(function(docRef) {
+//     console.log('Document written with ID: ', docRef.id);
+//   })
+//   .catch(function(error) {
+//     console.error('Error adding document: ', error);
+//   });
 //   });
 
 //   it('read from the database', () => {
