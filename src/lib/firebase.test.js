a87e29c2314423fe8b8f2cd373788413f703797a
Scott Bergler <commandinghands@gmail.com>
2020-04-08 09:53:30 -0700

    Add TestList component.

--- src/lib/firebase.test.js
+++ src/lib/firebase.test.js
@@ -1,31 +1,57 @@
-import { db } from './firebase';
+import React from 'react';
+import { FirestoreCollection } from 'react-firestore';
 
-describe('firebase reads and writes', () => {
-  it('writes to the database', () => {
-    let collectionName = 'testitems';
-    let groceryItem = {
-      name: 'limes',
-      category: 'citrus',
-    };
-    return db
-      .collection(collectionName)
-      .add(groceryItem)
-      .then(function(docRef) {
-        console.log('Document written with ID: ', docRef.id);
-      })
-      .catch(function(error) {
-        console.error('Error adding document: ', error);
-      });
-  });
+function TestList() {
+  return (
+    <FirestoreCollection
+      path="testitems"
+      sort="name:desc"
+      render={({ data }) => {
+        return (
+          <div>
+            <h1>Test Items</h1>
+            <ul>
+              {data.map(item => (
+                <li key={item.id}>
+                  {item.category} - {item.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      }}
+    />
+  );
+}
 
-  it('read from the database', () => {
-    return db
-      .collection('testitems')
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          console.log(`${doc.id} => ${doc.data().name}`);
-        });
-      });
-  });
-});
+export default TestList;
+
+// describe('firebase reads and writes', () => {
+//   it('writes to the database', () => {
+//     let collectionName = 'testitems';
+//     let groceryItem = {
+//       name: 'limes',
+//       category: 'citrus',
+//     };
+//     return db
+//       .collection(collectionName)
+//       .add(groceryItem)
+//       .then(function(docRef) {
+//         console.log('Document written with ID: ', docRef.id);
+//       })
+//       .catch(function(error) {
+//         console.error('Error adding document: ', error);
+//       });
+//   });
+
+//   it('read from the database', () => {
+//     return db
+//       .collection('testitems')
+//       .get()
+//       .then(querySnapshot => {
+//         querySnapshot.forEach(doc => {
+//           console.log(`${doc.id} => ${doc.data().name}`);
+//         });
+//       });
+//   });
+// });
