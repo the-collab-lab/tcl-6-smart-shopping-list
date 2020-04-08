98af086b02b0afbb2e2cdd87ce9e31283987fdbb
Scott Bergler <commandinghands@gmail.com>
2020-04-08 09:50:53 -0700

    Remove mocking test logic.

--- src/lib/firebase.test.js
+++ src/lib/firebase.test.js
@@ -1,61 +1,31 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
 import { db } from './firebase';
-import FirestoreMock from './firebase-mock';
 
 describe('firebase reads and writes', () => {
-  const firestoreMock = new FirestoreMock();
-  beforeEach(() => {
-    firebase.firestore = firestoreMock;
-    firestoreMock.reset();
-  });
-
-  it('does something', done => {
-    firestoreMock.mockSetReturn = { id: 'WkSiIbtG5SWOeu30VXz3' };
-    firebase.firestore
-      .collection('items')
-      .doc('WkSiIbtG5SWOeu30VXz3')
-      .set({ name: 'apples', category: 'produce' })
-      .then(res => {
-        expect(firestoreMock.mockCollection).toBeCalledWith('items');
-        expect(firestoreMock.mockSet).toBeCalledWith({
-          name: 'apples',
-          category: 'produce',
-        });
-        expect(res.id).toEqual('WkSiIbtG5SWOeu30VXz3');
-        done();
-      })
-      .catch(done);
-  });
-
-  let promiseVoid = Promise.resolve();
-  it('writes to database', () => {
-    let items = db
-      .collection('items')
-      .add({
-        name: 'blueberries',
-        categorey: 'fruit',
-      })
-      .then(function() {
-        console.log('Document written with ID');
+  it('writes to the database', () => {
+    let collectionName = 'testitems';
+    let groceryItem = {
+      name: 'limes',
+      category: 'citrus',
+    };
+    return db
+      .collection(collectionName)
+      .add(groceryItem)
+      .then(function(docRef) {
+        console.log('Document written with ID: ', docRef.id);
       })
       .catch(function(error) {
         console.error('Error adding document: ', error);
       });
-
-    expect(items).toEqual(promiseVoid);
-    // return items;
   });
 
-  it('reads from database', () => {
-    let items = db
-      .collection('items')
+  it('read from the database', () => {
+    return db
+      .collection('testitems')
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          console.log(`Item: ${doc.id} => ${doc.data().name}`);
+          console.log(`${doc.id} => ${doc.data().name}`);
         });
       });
-    return items;
   });
 });
