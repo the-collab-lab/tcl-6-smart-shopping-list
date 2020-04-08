bc4cbd30309caf88a1626b341d151b5769695dec
Scott Bergler <commandinghands@gmail.com>
2020-04-08 11:50:16 -0700

    Remove unused tests.

--- src/lib/firebase.test.js
+++ src/lib/firebase.test.js
@@ -101,33 +101,3 @@ function TestList() {
 }
 
 export { TestList, AddTestItem };
-
-// describe('firebase reads and writes', () => {
-//   it('writes to the database', () => {
-//     let collectionName = 'testitems';
-//     let groceryItem = {
-//       name: 'limes',
-//       category: 'citrus',
-//     };
-// return db
-//   .collection(collectionName)
-//   .add(groceryItem)
-//   .then(function(docRef) {
-//     console.log('Document written with ID: ', docRef.id);
-//   })
-//   .catch(function(error) {
-//     console.error('Error adding document: ', error);
-//   });
-//   });
-
-//   it('read from the database', () => {
-//     return db
-//       .collection('testitems')
-//       .get()
-//       .then(querySnapshot => {
-//         querySnapshot.forEach(doc => {
-//           console.log(`${doc.id} => ${doc.data().name}`);
-//         });
-//       });
-//   });
-// });
