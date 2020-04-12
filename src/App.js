452b1be4dc4e44504a3ce7176c45201e5b754e10
Sara Krum <sarasmith887@gmail.com>
2020-04-12 14:48:49 -0400

    new page components added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import NavButton from './components/navbutton.js';
+import Shopping from './components/shopping.js';
+import AddItem from './components/addItem.js';
 
 function App() {
   return (
---
+++ src/components/shopping.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+class Shopping extends React.Component {
+  render() {
+    return <h1>Shopping List</h1>;
+  }
+}
+
+export default Shopping;
