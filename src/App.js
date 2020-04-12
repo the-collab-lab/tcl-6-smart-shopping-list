551033f902da6e2341d9561636a3ce7db18355fa
Sara Krum <sarasmith887@gmail.com>
2020-04-12 11:48:51 -0400

    removing unused imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import NavButton from './components/navbutton.js';
 
