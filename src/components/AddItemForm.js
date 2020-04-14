e72c9991cd62dbf0ddac8334e36b60b6308843e9
Elise Spain <elisespain@gmail.com>
2020-04-14 14:26:54 -0700

    Updated the state for 'AddItemForm'

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -5,20 +5,36 @@ class AddItemForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Item: '',
+      name: '',
+      next_purchase: null,
+      last_purchase: null,
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.name);
+    event.preventDefault();
   }
 
   render() {
     return (
-      <form>
-        <label>
-          Item:
-          <input type="text" value={this.state.value} />
-          // need to add onChange but need to complete more research
-        </label>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
         <input type="submit" value="Submit" />
       </form>
     );
   }
 }
+
+export default AddItemForm;
