e0a69719aa686dcf84bfe9ca80015c7033959636
Sara Krum <sarasmith887@gmail.com>
2020-04-14 11:15:06 -0400

    created constant for user_token

--- src/components/shopping.js
+++ src/components/shopping.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
+import { USER_TOKEN } from '../../src/constants';
 import getToken from '../../src/tokenScript';
 import TestList from '../lib/testList';
 import '../App.css';
 
 function Shopping() {
-  const [user_token, refreshToken] = useState(
-    localStorage.getItem('user_token'),
-  );
+  const [user_token, refreshToken] = useState(localStorage.getItem(USER_TOKEN));
   function handleClick() {
-    localStorage.setItem('user_token', getToken());
-    refreshToken(localStorage.getItem('user_token'));
+    localStorage.setItem(USER_TOKEN, getToken());
+    refreshToken(localStorage.getItem(USER_TOKEN));
   }
   if (user_token) {
     return <TestList />;
