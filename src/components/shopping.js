23162d4c810e887cd4c17df28652d96621aaae4b
Sara Krum <sarasmith887@gmail.com>
2020-04-14 12:20:52 -0400

    WIP; add custom useToken hook

--- src/components/shopping.js
+++ src/components/shopping.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { USER_TOKEN } from '../../src/constants';
 import getToken from '../../src/tokenScript';
 import TestList from '../lib/testList';
+import { useToken } from '../lib/useToken';
 import '../App.css';
 
 function Shopping() {
-  const [userToken, refreshToken] = useState(() =>
-    localStorage.getItem(USER_TOKEN),
-  );
+  const userToken = useToken();
   function handleClick() {
     const token = getToken();
     localStorage.setItem(USER_TOKEN, token);
-    refreshToken(token);
+    //refreshToken(token);
   }
   if (userToken) {
     return <TestList />;
---
+++ src/lib/useToken.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { USER_TOKEN } from '../../src/constants';
+
+export function useToken() {
+  const [userToken, refreshToken] = useState(() =>
+    localStorage.getItem(USER_TOKEN),
+  );
+
+  useEffect(() => refreshToken(userToken), [userToken]);
+
+  return userToken;
+}
