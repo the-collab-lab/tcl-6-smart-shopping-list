ef2ba3b43294bb59daeb67aa1e83ba6a7e25dc51
Sara Krum <sarasmith887@gmail.com>
2020-04-14 12:59:05 -0400

    WIP: show/hide nav buttons

--- src/App.js
+++ src/App.js
@@ -4,21 +4,36 @@ import './App.css';
 import NavButton from './components/navbutton.js';
 import Shopping from './components/shopping.js';
 import AddItem from './components/addItem.js';
+import { useToken } from './lib/useToken';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Shopping} />
-        <Route path="/add" component={AddItem} />
+  const [userToken, refreshToken] = useToken();
+  console.log(`user token: ${userToken}`);
+  if (userToken) {
+    refreshToken();
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Shopping} />
+          <Route path="/add" component={AddItem} />
 
-        <nav>
-          <NavButton path="/" text="Shopping" />
-          <NavButton path="/add" text="Add Item" />
-        </nav>
-      </div>
-    </Router>
-  );
+          <nav id="nav">
+            <NavButton path="/" text="Shopping" />
+            <NavButton path="/add" text="Add Item" />
+          </nav>
+        </div>
+      </Router>
+    );
+  } else {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Shopping} />
+          <Route path="/add" component={AddItem} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
