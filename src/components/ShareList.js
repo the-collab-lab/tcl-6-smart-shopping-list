7c978ae28def7592a8534f3a714849f1d5bd31dc
Janet Azehko <Janet@projecterostribe.com>
2020-04-21 14:40:51 -0700

    create shared list  error message

--- src/components/ShareList.js
+++ src/components/ShareList.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withFirestore } from 'react-firestore';
 
 const ShareList = () => {
+  const [isError, toggleError] = useState(false);
   const handleSubmit = event => {
     event.preventDefault();
+    toggleError(!isError);
     console.log(event.target.value);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Share List:
-        <input type="text" name="token" />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Share List:
+          <input type="text" name="token" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div>{isError ? <p>ERROR</p> : null}</div>
+    </div>
   );
 };
 
