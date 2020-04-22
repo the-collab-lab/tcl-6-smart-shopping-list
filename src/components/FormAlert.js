a7a5abd8edad59209b0b6db24ef6d202ebd59e8f
Elise Spain <elisespain@gmail.com>
2020-04-22 11:46:05 -0700

    Setup 'NormalizeItem' function

--- src/components/FormAlert.js
+++ src/components/FormAlert.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { FirestoreCollection } from 'react-firestore';
 
+function normalizeItem(item) {
+  var pattern = /\d[.,\/#!$%\^&\*;:{}=\-_`~()]\ix/;
+  // remove capitilization
+  item.replace(pattern, '');
+  return;
+  // remove numbers and special characters
+  // spaces
+  //return item normalized
+}
+
 function FormAlert(props) {
   return (
     <FirestoreCollection
       path="items"
       filter={['name', '==', props.userSubmit]}
       render={({ data }) => {
-        console.log(data);
-        console.log(data.length);
-
         if (data.length > 0) {
-          return <h1>Alert :Get it together</h1>;
+          return (
+            <p>This item has already been added to your shopping list.</p> // if we can add the actual item name {data.name}
+          );
         } else {
           return <div></div>; //Is there a better way to have an empty return
         }
