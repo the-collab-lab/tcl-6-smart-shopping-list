e53094b19654b9318825c1e79ffa5d1a1e7c5ec2
Elise Spain <elisespain@gmail.com>
2020-04-27 13:04:10 -0700

    WIP: Tested time functionality of purchase button

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../CSS/ListItem.css';
 
 const ListItem = item => {
+  const [isPurchased, setPurchased] = useState(false);
+
+  let buttonClass = isPurchased ? 'purchased' : 'not-purchased';
+
+  function onHandle(event) {
+    setPurchased(!isPurchased);
+    onPurchase();
+    event.preventDefault();
+  }
+
+  function onPurchase() {
+    setTimeout(() => {
+      setPurchased(false);
+    }, 4000);
+    console.log(setTimeout);
+  }
+
   return (
     <li>
       {item.name} / {item.next_purchase}
-      <button>Purchased</button>
+      <button className={buttonClass} onClick={onHandle}>
+        Purchase
+      </button>
     </li>
   );
 };
