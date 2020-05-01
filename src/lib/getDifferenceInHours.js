7b4122c69de64b57a1341bc7c302edc89b16edec
Scott Bergler <commandinghands@gmail.com>
2020-04-30 18:41:07 -0700

    Add explanatory comment.

--- src/lib/getDifferenceInHours.js
+++ src/lib/getDifferenceInHours.js
@@ -1,6 +1,7 @@
 import { fakeCurrentTime } from '../timeConstants';
 
 export function getDifferenceInHours(last_purchased) {
+  // REMOVE fakeCurrentTime TO GET THE TRUE CURRENT DATE/TIME.
   let nowTimeStamp = new Date(fakeCurrentTime).getTime();
   let lastPurchaseTimeStamp = new Date(last_purchased).getTime();
 
