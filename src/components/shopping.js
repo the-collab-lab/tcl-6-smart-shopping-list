66c1366e5a2c92699f692c73ded498061aeaa9ba
Sara Krum <sarasmith887@gmail.com>
2020-05-03 19:40:21 -0400

    un needed call to the db removed

--- src/components/shopping.js
+++ src/components/shopping.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import '../App.css';
-import { ITEMS, USER_TOKEN } from '../constants';
-import { FirestoreCollection } from 'react-firestore';
+import { ITEMS } from '../constants';
 import { db } from '../lib/firebase';
+
 function Shopping(props) {
   function handleClick(id) {
     db.collection(ITEMS)
@@ -16,42 +15,34 @@ function Shopping(props) {
       });
   }
 
-  return (
-    <FirestoreCollection
-      path={ITEMS}
-      filter={[USER_TOKEN, '==', props.userToken]}
-      render={({ data }) => {
-        if (data.length > 0) {
-          return (
-            <div>
-              <h1>Shopping List</h1>
-              <h2>***Click item to delete from cart***</h2>
+  if (props.list.length > 0) {
+    return (
+      <div>
+        <h1>Shopping List</h1>
+        <h2>***Click item to delete from cart***</h2>
 
-              <ul>
-                {data.map(item => (
-                  <li
-                    key={item.id}
-                    onClick={() => {
-                      return handleClick(item.id);
-                    }}
-                  >
-                    {item.name} / {item.next_purchase}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        } else {
-          return (
-            <div>
-              <h1> Shopping List</h1>
-              <p>Theres nothing in your cart, please buy our stuff.</p>
-            </div>
-          );
-        }
-      }}
-    />
-  );
+        <ul>
+          {props.list.map(item => (
+            <li
+              key={item.id}
+              onClick={() => {
+                return handleClick(item.id);
+              }}
+            >
+              {item.name} / {item.next_purchase}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1> Shopping List</h1>
+        <p>Theres nothing in your cart, please buy our stuff.</p>
+      </div>
+    );
+  }
 }
 
 export default Shopping;
