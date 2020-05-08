1dae3796176ee42d52eb4d1a78fd45454bcf6610
Janet Azehko <Janet@projecterostribe.com>
2020-05-08 11:48:45 -0700

    WIP input onChange event map property undefined.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,39 @@
 import React, { useState } from 'react';
 
 const Filter = props => {
-  function handleTextChange() {
-    console.log('Hey you typed something');
-  }
+  const [word, setWord] = useState('');
+  const [filterDisplay, setFilterDisplay] = useState(props.items);
+
+  const handleTextChange = event => {
+    //console.log(props.item);
+    let oldList = props.items.map(item => {
+      return {
+        item: item.name.toLowerCase(),
+      };
+    });
+    if (event !== '') {
+      let newList = [];
+      setWord(event);
+      newList = oldList.filter(item => item.name.includes(word.toLowerCase()));
+      setFilterDisplay(newList);
+    } else {
+      setFilterDisplay(props.item);
+    }
+  };
 
   return (
-    <input onChange={handleTextChange} type="text" placeholder="Search"></input>
+    <div>
+      <input
+        onChange={event => handleTextChange(event.target.value)}
+        type="text"
+        placeholder="Search"
+      />
+      {filterDisplay.map((item, i) => (
+        <div>
+          <li>{item.name}</li>
+        </div>
+      ))}
+    </div>
   );
 };
 
