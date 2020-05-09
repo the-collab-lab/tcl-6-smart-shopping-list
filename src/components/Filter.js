c0e2684ba49a65eb93d1fc8e0922b564b6f3fa8b
Janet Azehko <Janet@projecterostribe.com>
2020-05-09 15:06:23 -0700

    AC reached

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,38 +1,15 @@
 import React, { useState } from 'react';
 
-const Filter = props => {
-  const [word, setWord] = useState('');
-  const [filterDisplay, setFilterDisplay] = useState(props.items);
-
-  const handleTextChange = event => {
-    //console.log(props.item);
-    let oldList = props.items.map(item => {
-      return {
-        item: item.name.toLowerCase(),
-      };
-    });
-    if (event !== '') {
-      let newList = [];
-      setWord(event);
-      newList = oldList.filter(item => item.name.includes(word.toLowerCase()));
-      setFilterDisplay(newList);
-    } else {
-      setFilterDisplay(props.item);
-    }
-  };
-
+const Filter = ({ value, onChange, clear }) => {
   return (
     <div>
       <input
-        onChange={event => handleTextChange(event.target.value)}
         type="text"
         placeholder="Search"
+        value={value}
+        onChange={onChange}
       />
-      {filterDisplay.map((item, i) => (
-        <div>
-          <li>{item.name}</li>
-        </div>
-      ))}
+      <button onClick={clear}>CLEAR</button>
     </div>
   );
 };
