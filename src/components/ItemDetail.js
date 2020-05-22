ff8696b23a04515f7de85293b8a21d324159417d
Scott Bergler <commandinghands@gmail.com>
2020-05-22 09:40:58 -0700

    Make sure the next purchase date is a valid date.

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -7,7 +7,10 @@ const ItemDetail = ({ list }) => {
   let { itemName } = useParams();
   let item = list.find(item => item.name_normalized === itemName);
 
-  let lastPurchase = item ? new Date(item.last_purchased).toDateString() : '';
+  let lastPurchase =
+    item && item.last_purchased !== ''
+      ? new Date(item.last_purchased).toDateString()
+      : '';
 
   return item ? (
     <main>
@@ -17,14 +20,14 @@ const ItemDetail = ({ list }) => {
           src={arrowLeft}
           alt="Back to shopping list"
         />{' '}
-        <span className="back-text">Back to shopping</span>
+        <span className="back-text">Back to list</span>
       </Link>
 
       <h1>Item Details</h1>
       <h2>Name: {item.name}</h2>
-      <h2>Last Purchased: {lastPurchase}</h2>
-      <h2>Next Purchase: {item.next_purchase} days</h2>
       <h2>Number of Purchases: {item.number_purchases}</h2>
+      <h2>You might want this in about: {item.next_purchase} days</h2>
+      {lastPurchase === '' ? null : <h2>Last Purchased: {lastPurchase}</h2>}
     </main>
   ) : (
     <>
